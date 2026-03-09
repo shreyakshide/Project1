e2f9584f63e55b117a356fdff0120d14f3ae0473
shreyakshide <shreyakshi@gmail.com>
2026-03-09 12:39:14 +0530

    update

--- src/com/git/Demo.java
+++ src/com/git/Demo.java
@@ -3,7 +3,7 @@ package com.git;
 public class Demo {
 
 	public static void main(String[] args) {
-		int a=10;
+		int a=20;
 		int b=20;
 		System.out.println(a+b);
 		// TODO Auto-generated method stub
